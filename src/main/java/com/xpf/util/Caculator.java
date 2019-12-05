fcf0574d0ec8351c8818a0ffdb93d9d3f44de5fd
LiYuFuHua <512918623@qq.com>
2019-12-05 10:01:09 +0800

    这是一个junit的Git测试项目

---
+++ src/main/java/com/xpf/util/Caculator.java
@@ -0,0 +1,22 @@
+package com.xpf.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Caculator {
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	public double sub(double a, double b) {
+		return a - b;
+	}
+
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+
+	public double div(double a, double b) {
+		return a / b;
+	}
+}
